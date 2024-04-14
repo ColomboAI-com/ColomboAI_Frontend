369b227fa7e5646075f7a2a214a014376b16b3ee
Kinay Dhanani <kinay.dhanani07@gmail.com>
2024-04-14 23:51:30 +0530

    get users post function add

--- src/context/FeedContext.js
+++ src/context/FeedContext.js
@@ -17,7 +17,8 @@ export default function FeedContextProvider({ children }) {
     addComment: false,
     deleteComment: false,
     generatePost: false,
-    generateComment: false
+    generateComment: false,
+    GetUserPost: false
   })
 
   const getPosts = async () => {
@@ -134,7 +135,7 @@ export default function FeedContextProvider({ children }) {
 
   const rePost = async (postId = '') => {
     try {
-      const res = await axios.put(`${ROOT_URL_FEED}/post/re-post/${postId}`,
+      const res = await axios.patch(`${ROOT_URL_FEED}/post/re-post/${postId}`,
         null,
         {
           headers: {
@@ -189,6 +190,24 @@ export default function FeedContextProvider({ children }) {
     }
   }
 
+  const getPostsOfUser = async (username = '') => {
+    try {
+      setLoadings(prev => ({ ...prev, GetUserPost: true }))
+      const res = await axios.get(`${ROOT_URL_FEED}/post/user/${username}`,
+        {
+          headers: {
+            Authorization: getCookie('token')
+          }
+        }
+      )
+      return res.data
+    } catch (err) {
+      handleError(err)
+    } finally {
+      setLoadings(prev => ({ ...prev, GetUserPost: false }))
+    }
+  }
+
   return (
     <FeedContext.Provider value={{
       posts, setPosts,
@@ -196,7 +215,8 @@ export default function FeedContextProvider({ children }) {
       createPost, deletePost,
       likePost, rePost,
       addComment, deleteComment,
-      generatePost, generateComment
+      generatePost, generateComment,
+      getPostsOfUser
     }}>
       {children}
     </FeedContext.Provider>
