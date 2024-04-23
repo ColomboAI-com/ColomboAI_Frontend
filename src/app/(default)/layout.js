1e33bcb1140bb6a9e07495781f0199bc632e0db0
Aman-Sonwani <amansonwani45@gmail.com>
2024-04-23 17:27:48 +0530

    bottombar links added and page height fixed

--- src/app/(default)/layout.js
+++ src/app/(default)/layout.js
@@ -78,7 +78,7 @@ const DefaultLayout = ({ children }) => {
           <div className="min-w-[100%] md:min-w-[90%] xl:min-w-[95%] flex flex-col">
             <Header />
             <div className="flex flex-1 border- border-purple-400">
-              <div className="overflow-y-auto lg:w-[70%] min-h-[60vh] px-6 md:px-10 lg:px-20">
+              <div className="overflow-y-auto lg:w-[70%] min-h-[75vh] px-6 md:px-10 lg:px-20">
                 {children}
               </div>
               <div className="hidden overflow-hidden lg:block lg:w-[30%] pt-10 px-2 shadow-[0px_2px_4px_0px_#0000001A]">
@@ -92,36 +92,50 @@ const DefaultLayout = ({ children }) => {
         <div className=" md:hidden bg-white sticky bottom-0 z-50 border-t-2 border-brandprimary rounded-xl">
             <div className="shadow-[0px_2px_4px_0px_#0000001A]">
                 <div className="py-4 flex flex-wrap items-center justify-evenly">
-                  <div className="mx-4">
-                      <div className="w-[29px] mx-auto">
-                          <GenAiIcon w="30" h="30" fill={pathname === '/gen-ai' ? "#1E71F2" : "#8E8E93"} />
-                      </div>
-                      <p className={`${pathname === '/gen-ai' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Gen AI</p>
-                  </div>
-                  <div className="mx-4">
-                      <div className="w-[29px] mx-auto">
-                          <TaskBotIcon w="30" h="30" fill={pathname === '/task-bot' ? "#1E71F2" : "#8E8E93"} />
-                      </div>
-                      <p className={`${pathname === '/task-bot' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Task bot</p>
-                  </div>
-                  <div className="mx-4">
-                      <div className="w-[29px] mx-auto">
-                          <FeedIcon w="30" h="30" fill={pathname.includes('/feed') ? "#1E71F2" : "#8E8E93"} />
-                      </div>
-                      <p className={`${pathname.includes('/feed') ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Feed</p>
-                  </div>
-                  <div className="mx-4">
-                      <div className="w-[29px] mx-auto">
-                          <ShopIcon w="30" h="30" fill={pathname === '/shop' ? "#1E71F2" : "#8E8E93"} />
-                      </div>
-                      <p className={`${pathname === '/shop' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Shop</p>
-                  </div>
-                  <div className="mx-4">
-                      <div className="w-[29px] mx-auto">
-                          <NewsIcon w="30" h="30" fill={pathname === '/news' ? "#1E71F2" : "#8E8E93"} />
-                      </div>
-                      <p className={`${pathname === '/news' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>News</p>
-                  </div>
+                <Link href="/gen-ai">
+                    <div className="mx-4">
+                        <div className="w-[29px] mx-auto">
+                            <GenAiIcon w="30" h="30" fill={pathname === '/gen-ai' ? "#1E71F2" : "#8E8E93"} />
+                        </div>
+                        <p className={`${pathname === '/gen-ai' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Gen AI</p>
+                    </div>
+                </Link>
+
+                <Link href="/task-bot">
+                    <div className="mx-4">
+                        <div className="w-[29px] mx-auto">
+                            <TaskBotIcon w="30" h="30" fill={pathname === '/task-bot' ? "#1E71F2" : "#8E8E93"} />
+                        </div>
+                        <p className={`${pathname === '/task-bot' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Task bot</p>
+                    </div>
+                </Link>
+
+                <Link href="/feed">
+                    <div className="mx-4 ">
+                        <div className="w-[29px] mx-auto">
+                            <FeedIcon w="30" h="30" fill={pathname.includes('/feed') ? "#1E71F2" : "#8E8E93"} />
+                        </div>
+                        <p className={`${pathname.includes('/feed') ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Feed</p>
+                    </div>
+                </Link>
+
+                <Link href="/shop">
+                    <div className="mx-4">
+                        <div className="w-[29px] mx-auto">
+                            <ShopIcon w="30" h="30" fill={pathname === '/shop' ? "#1E71F2" : "#8E8E93"} />
+                        </div>
+                        <p className={`${pathname === '/shop' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>Shop</p>
+                    </div>
+                </Link>
+
+                <Link href="/news">
+                    <div className="mx-4">
+                        <div className="w-[29px] mx-auto">
+                            <NewsIcon w="30" h="30" fill={pathname === '/news' ? "#1E71F2" : "#8E8E93"} />
+                        </div>
+                        <p className={`${pathname === '/news' ? "text-brandprimary" : "text-sidebaricon"} text-center text-[14px] mt-3 font-sans`}>News</p>
+                    </div>
+                </Link>
                 </div>
             </div>
         </div>
