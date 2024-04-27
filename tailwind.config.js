472296734eed7128a1e403eda5e246e60362eb36
pankaj310 <chaure.pankaj31@gmail.com>
2024-04-28 01:02:42 +0530

    config file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
       'sm': {'min': '320px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      'sm2': {'min':'600px', 'max': '767px'},
+      // 'sm2': {'min':'600px', 'max': '767px'},
 
       'md': {'min': '768px'},
       // // => @media (min-width: 768px and max-width: 1023px) { ... }
@@ -36,7 +36,7 @@ module.exports = {
       'xl': {'min': '1280px'},
       // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-      'xxl': {'min': '1920px', 'max': '2560px'},
+      // 'xxl': {'min': '1920px', 'max': '2560px'},
         //insert styles here...
     
       '2xl': {'min': '1536px'},
