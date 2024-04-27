fd60d7051ec0552160bea354e1c5c588f71dc8da
kinaydhanani <kinay.dhanani07@gmail.com>
2024-04-27 12:10:46 +0530

    firebase config change

--- src/utlils/firebaseConfig.js
+++ src/utlils/firebaseConfig.js
@@ -7,8 +7,8 @@ const firebaseConfig = {
   projectId: "colomboai-97e08",
   storageBucket: "colomboai-97e08.appspot.com",
   messagingSenderId: "462941437494",
-  appId: "1:462941437494:web:ed842e957861b8558ba297",
-  measurementId: "G-6C39N4PLTC"
+  appId: "1:462941437494:web:e0e74557910fba5c8ba297",
+  measurementId: "G-X34XVSEPY5"
 }
 
 const app = initializeApp(firebaseConfig)
