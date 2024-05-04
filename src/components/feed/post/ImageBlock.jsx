98fb998085281a828ef031779bb916213403ec94
pankaj310 <chaure.pankaj31@gmail.com>
2024-05-04 21:50:49 +0530

    feed post UI updated

--- src/components/feed/post/ImageBlock.jsx
+++ src/components/feed/post/ImageBlock.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 export default function ImageBlock({ image = '/images/home/feed-banner-img.png' }) {
   return (
-    <div>
-      <img src={image} alt="post_image" className="w-full aspect-video object-fill bg-gray-100" />
+    <div className="min-h-[466px] max-h-[466px]">
+      <img src={image} alt="post_image" className="w-full h-full aspect-video object-fill bg-gray-100" />
     </div>
   )
 }
