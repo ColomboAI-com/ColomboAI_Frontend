6094b29967e6676841592e3387c195b0f9708dcf
pankaj310 <chaure.pankaj31@gmail.com>
2024-05-04 22:46:12 +0530

    slider responsive set

--- src/components/elements/sliders/Stories.jsx
+++ src/components/elements/sliders/Stories.jsx
@@ -11,32 +11,30 @@ var settings = {
     slidesToScroll: 1,
     responsive: [
         {
-            breakpoint: 1200,
-            setting: {
-                slidesToShow: 5,
-            },
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            // infinite: true,
+            // dots: true
+          }
         },
         {
-            breakpoint: 1000,
-            setting: {
-                slidesToShow: 5,
-            },
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
         },
         {
-            breakpoint: 600,
-            setting: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-            },
-        },
-        {
-            breakpoint: 480,
-            setting: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-            },
-        },
-    ],
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        }
+    ]
 };
 
 const Stories = () => {
