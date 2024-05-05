5ad1019f06bd5374b67cac3200533b8527d97f90
mirajpatel752 <mirajpatel752@gmail.com>
2024-05-05 16:40:26 +0530

    notificationservice

--- src/components/notifications/ActivitiesList.jsx
+++ src/components/notifications/ActivitiesList.jsx
@@ -11,12 +11,12 @@ const ActivitiesList = () => {
         <div className="text-sm text-navbaraction">
             {mentions.map(mention => (
                 <div key={mention.id} className="flex items-center p-2 border rounded space-x-4">
-                    <img src={mention.profilePic} alt="Profile" className="w-12 h-12 rounded-full" />
+                    <img src={mention.profilePic} alt="Profile" className="w-[46px] h-[46px] rounded-full" />
                     <div className="flex-grow">
                         <strong>{mention.user}</strong> {mention.message}
                     </div>
-                    <img src={mention.postImage} alt="Post" className="w-12 h-12 rounded" />
-                    <button className={"px-4 py-1 text-white rounded bg-brandprimary"}>
+                    <img src={mention.postImage} alt="Post" className="w-[33px] h-[33px] rounded" />
+                    <button className={"px-3 py-1 text-white rounded bg-brandprimary"}>
                         {mention.followType}
                     </button>
                 </div>
