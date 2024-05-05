32f2fc84feee9ad3785f521830a80d60e822aee7
Kinay Dhanani <kinay.dhanani07@gmail.com>
2024-05-06 00:28:20 +0530

    redirection improvement

--- src/app/(default)/page.js
+++ src/app/(default)/page.js
@@ -1,8 +1,13 @@
 'use client'
 import { getCookie } from "cookies-next"
 import { redirect } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
-  if (getCookie('token')) redirect('/feed')
-  else redirect('/sign-up')
+
+  useEffect(() => {
+    if (getCookie('token')) redirect('/feed')
+    else redirect('/sign-up')
+  }, [])
+
 }
