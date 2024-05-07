6f0a5456375330d361c9ec1bdb512dd422ec1cc2
ajinkyabhise <ajinkyabhise1226@gmail.com>
2024-05-07 21:06:25 +0530

    changes

--- src/components/story/SingleStoryModal.jsx
+++ src/components/story/SingleStoryModal.jsx
@@ -26,7 +26,7 @@ export default function SingleStoryModal({ setIsCreateStorySignleOpen, data}) {
     <div className="bg-[#1a1a1af0] p-4 grid grid-cols-3">
       {/* Left Section */}
       <div className="flex justify-end p-4 text-3xl">
-        <MdOutlineArrowBack onClick={() => setIsCreateStorySignleOpen(false)} />
+        <MdOutlineArrowBack onClick={() => setIsCreateStorySignleOpen(false)} style={{ color: '#fff' }}/>
       </div>
 
       {/* Middle Section */}
@@ -74,6 +74,7 @@ export default function SingleStoryModal({ setIsCreateStorySignleOpen, data}) {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 4 15"
             fill="currentColor"
+            style={{ color: '#fff' }}
           >
             <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
           </svg>
