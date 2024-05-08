b42bbfb208d7a29b21aeb055b87e0125cdd4af3a
Aman-Sonwani <amansonwani45@gmail.com>
2024-05-09 01:45:23 +0530

    bug fixing 2

--- src/components/feed/post/ImageBlock.jsx
+++ src/components/feed/post/ImageBlock.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 export default function ImageBlock({ image = '/images/home/feed-banner-img.png' }) {
   return (
-    <div className="min-h-[466px] max-h-[466px]">
-      <img src={image} alt="post_image" className="w-full h-full aspect-video object-fill bg-gray-100" />
+    <div className="">
+      <img src={image} alt="post_image" className="w-full h-full aspect-video object-contain bg-gray-100" />
     </div>
   )
 }
