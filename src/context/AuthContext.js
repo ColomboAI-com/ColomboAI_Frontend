74d0942ded4e3f367ed0c5a8d4713891aa30625d
xorvet <cypherx702@gmail.com>
2024-08-29 16:15:26 +0530

    new auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,7 +11,9 @@ const AuthContext = createContext()
 const defaultInputs = {
   username: '',
   name: '',
+  age: '',
   email: '',
+  country_code: '',
   phone: '',
   otp: ''
 }
@@ -19,7 +21,9 @@ const defaultInputs = {
 const defaultValidations = {
   username: false,
   name: false,
+  age: false,
   email: false,
+  country_code: false,
   phone: false,
   otp: false
 }
@@ -37,12 +41,13 @@ export const AuthContextProvider = ({ children }) => {
   const [inputs, setInputs] = useState(defaultInputs)
   const [validations, setValidations] = useState(defaultValidations)
   const [loadings, setLoadings] = useState(defaultLoadings)
+  const [new_device, setNew_device] = useState(false);
 
   const handleInputs = (event) => {
     const { name, value } = event.target
     if (name === "username" && !USERNAME_REGEX.test(value)) return
     if (name === "name" && !NAME_REGEX.test(value)) return
-    if ((name === "phone" || name === "otp") && !INT_NUMBER_REGEX.test(value)) return
+    if ((name === "phone" || name === "otp" || name === "age" || name === "country_code") && !INT_NUMBER_REGEX.test(value)) return
     setInputs(prev => ({ ...prev, [name]: value }))
     setValidations(prev => ({ ...prev, [name]: false }))
   }
@@ -50,13 +55,22 @@ export const AuthContextProvider = ({ children }) => {
   const getOTP = async (action = 'sign-in') => {
     try {
       setLoadings(prev => ({ ...prev, otp: true }))
-      const res = await axios.post(`${ROOT_URL_AUTH}/auth/get-otp`,
-        {
-          action,
-          email: inputs.email,
-          user_name: inputs.username
-        }
-      )
+      const data = (action === 'sign-in') ? {
+        action,
+        email: inputs.email,
+        user_name: inputs.username
+      } :
+      {
+        action,
+        email: inputs.email,
+        user_name: inputs.username,
+        name: inputs.name,
+        age: inputs.age,
+        email: inputs.email,
+        contact_number: inputs.phone,
+      }
+
+      const res = await axios.post(`${ROOT_URL_AUTH}/auth/get-otp`, data)
       MessageBox('success', res.data.message)
       return res
     } catch (err) {
@@ -68,12 +82,14 @@ export const AuthContextProvider = ({ children }) => {
 
   const signUp = async () => {
     try {
+      
       setLoadings(prev => ({ ...prev, auth: true }))
       const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-up`,
         {
           action: 'sign-up',
           user_name: inputs.username,
           name: inputs.name,
+          age: inputs.age,
           email: inputs.email,
           contact_number: inputs.phone,
           otp: inputs.otp
@@ -95,9 +111,13 @@ export const AuthContextProvider = ({ children }) => {
         {
           action: 'sign-in',
           email: inputs.email,
-          otp: inputs.otp
+          otp: inputs.otp,
+          phone_otp: inputs.phone_otp ? inputs.phone_otp : "" 
         }
       )
+      
+      setNew_device(res.data.new_device);
+
       MessageBox('success', res.data.message)
       return res
     } catch (err) {
@@ -122,6 +142,18 @@ export const AuthContextProvider = ({ children }) => {
     }
   }
 
+  const addnewdevice = async (email) => {
+    try {
+      const res = await axios.post(`${ROOT_URL_AUTH}/user/addnewdevice`,
+        { email }
+      )
+      MessageBox('success', res.data.message)
+      return res
+    } catch (error) {
+      handleError(err)
+    } 
+  }
+
   const resetAuthValues = () => {
     setInputs(prev => ({ ...prev, ...defaultInputs }))
     setValidations(prev => ({ ...prev, ...defaultValidations }))
@@ -136,6 +168,7 @@ export const AuthContextProvider = ({ children }) => {
       handleInputs,
       getOTP, signUp, signIn,
       ssoAuthentication,
+      addnewdevice,
       resetAuthValues
     }}>
       {children}
