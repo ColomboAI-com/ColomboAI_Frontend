07eda5050cf9ac0927138dc0b123208702a54c38
xorvet <cypherx702@gmail.com>
2024-09-01 03:20:19 +0530

    unicode fix

--- src/app/messages/page.js
+++ src/app/messages/page.js
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React, { useContext, useEffect } from "react"
 import { GlobalContext } from "@/context/GlobalContext"
 import UserProfileSection from "@/components/messages/UserProfileSection"
