525b449d5bdea3bbba1226302135af31b32549b9
sravani586 <sravsreddy586@gmail.com>
2024-09-03 23:54:49 -0400

    ADD device registration component

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,24 +1,294 @@
-'use client'
-import { createContext, useContext, useState } from "react"
-import { INT_NUMBER_REGEX, NAME_REGEX, USERNAME_REGEX } from "@/utlils/constant"
-import axios from "axios"
-import { ROOT_URL_AUTH } from "@/utlils/rootURL"
-import { handleError } from "@/utlils/handleError"
-import { MessageBox } from "@/components/MessageBox"
-import { getCookie } from "@/utlils/cookies"
+// "use client";
+// import { createContext, useContext, useState } from "react";
+// import {
+//   INT_NUMBER_REGEX,
+//   NAME_REGEX,
+//   USERNAME_REGEX,
+// } from "@/utlils/constant";
+// import axios from "axios";
+// import { ROOT_URL_AUTH } from "@/utlils/rootURL";
+// import { handleError } from "@/utlils/handleError";
+// import { MessageBox } from "@/components/MessageBox";
+// import { getCookie } from "@/utlils/cookies";
+// import NewDevicePopup from "@/components/auth/NewDevicePopup";
 
-const AuthContext = createContext()
+// const AuthContext = createContext();
+
+// const defaultInputs = {
+//   username: "",
+//   name: "",
+//   age: "",
+//   email: "",
+//   country_code: "",
+//   phone: "",
+//   otp: "",
+//   phone_otp: "",
+// };
+
+// const defaultValidations = {
+//   username: false,
+//   name: false,
+//   age: false,
+//   email: false,
+//   country_code: false,
+//   phone: false,
+//   otp: false,
+//   phone_otp: false,
+// };
+
+// const defaultLoadings = {
+//   otp: false,
+//   auth: false,
+//   google: false,
+//   meta: false,
+//   microsoft: false,
+// };
+
+// export const AuthContextProvider = ({ children }) => {
+//   const [inputs, setInputs] = useState(defaultInputs);
+//   const [validations, setValidations] = useState(defaultValidations);
+//   const [loadings, setLoadings] = useState(defaultLoadings);
+//   const [verifier, setVerifier] = useState();
+//   const [new_device, setNew_device] = useState(false);
+//   const [showPopup, setShowPopup] = useState(false);
+
+//   const handleInputs = (event) => {
+//     const { name, value } = event.target;
+//     if (name === "username" && !USERNAME_REGEX.test(value)) return;
+//     if (name === "name" && !NAME_REGEX.test(value)) return;
+//     if (
+//       (name === "phone" ||
+//         name === "otp" ||
+//         name === "age" ||
+//         name === "country_code" ||
+//         name === "phone_otp") &&
+//       !INT_NUMBER_REGEX.test(value)
+//     )
+//       return;
+//     setInputs((prev) => ({ ...prev, [name]: value }));
+//     setValidations((prev) => ({ ...prev, [name]: false }));
+//   };
+
+//   const getOTP = async (action = "sign-in") => {
+//     try {
+//       setLoadings((prev) => ({ ...prev, otp: true }));
+//       const data =
+//         action === "sign-in"
+//           ? {
+//               action,
+//               email: inputs.email,
+//             }
+//           : {
+//               action,
+//               email: inputs.email,
+//               user_name: inputs.username,
+//               name: inputs.name,
+//               age: inputs.age,
+//               email: inputs.email,
+//               contact_number: inputs.phone,
+//             };
+
+//       const res = await axios.post(`${ROOT_URL_AUTH}/auth/get-otp`, data);
+//       MessageBox("success", res.data.message);
+//       return res;
+//     } catch (err) {
+//       handleError(err);
+//     } finally {
+//       setLoadings((prev) => ({ ...prev, otp: false }));
+//     }
+//   };
+
+//   const signUp = async () => {
+//     try {
+//       setLoadings((prev) => ({ ...prev, auth: true }));
+//       const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-up`, {
+//         action: "sign-up",
+//         user_name: inputs.username,
+//         name: inputs.name,
+//         age: inputs.age,
+//         email: inputs.email,
+//         contact_number: inputs.phone,
+//         otp: inputs.otp,
+//         phone_otp: inputs.phone_otp,
+//       });
+//       MessageBox("success", res.data.message);
+//       return res;
+//     } catch (err) {
+//       handleError(err);
+//     } finally {
+//       setLoadings((prev) => ({ ...prev, auth: false }));
+//     }
+//   };
+
+//   const signIn = async () => {
+//     try {
+//       setLoadings((prev) => ({ ...prev, auth: true }));
+
+//       const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-in`, {
+//         action: "sign-in",
+//         email: inputs.email,
+//         otp: inputs.otp,
+//         phone_otp: inputs.phone_otp,
+//       });
+
+//       // if (res.data.new_device) {
+//       // setNew_device(res.data.new_device);
+//       // await addnewdevice(inputs.email); // Add new device after sign-in
+//       // setShowPopup(true); // Show the popup after the new device is added
+//       // }
+
+//       // console.log("Sign-in response:", res.data);
+
+//       // setNew_device(res.data.new_device);
+
+//       // console.log("Calling addnewdevice...");
+//       // await addnewdevice(inputs.email); // Add new device after sign-in
+//       // console.log("addnewdevice should have been called.");
+
+//       // setShowPopup(true); // Show the popup after the new device is added
+//       // console.log("Popup should be set to show.");
+
+//       setShowPopup(true);
+//       MessageBox("success", res.data.message);
+//       return res;
+//     } catch (err) {
+//       handleError(err);
+//     } finally {
+//       setLoadings((prev) => ({ ...prev, auth: false }));
+//     }
+//   };
+
+//   const ssoAuthentication = async ({ provider, token }) => {
+//     try {
+//       setLoadings((prev) => ({ ...prev, [provider]: true }));
+//       const res = await axios.post(`${ROOT_URL_AUTH}/auth/sso-authentication`, {
+//         provider,
+//         token,
+//       });
+//       MessageBox("success", res.data.message);
+//       return res;
+//     } catch (err) {
+//       handleError(err);
+//     } finally {
+//       setLoadings((prev) => ({ ...prev, [provider]: false }));
+//     }
+//   };
+
+//   const addnewdevice = async (email) => {
+//     try {
+//       const res = await axios.post(
+//         `${ROOT_URL_AUTH}/user/addnewdevice`,
+//         { email },
+//         {
+//           headers: {
+//             Authorization: getCookie("token"),
+//           },
+//         }
+//       );
+//       MessageBox("success", res.data.message);
+//       return res;
+//     } catch (error) {
+//       handleError(err);
+//     }
+//   };
+
+//   const resetAuthValues = () => {
+//     setInputs((prev) => ({ ...prev, ...defaultInputs }));
+//     setValidations((prev) => ({ ...prev, ...defaultValidations }));
+//     setLoadings((prev) => ({ ...prev, ...defaultLoadings }));
+//   };
+
+//   const handlePopupConfirm = async () => {
+//     setShowPopup(false); // Hide the popup after confirming
+//   };
+
+//   const handlePopupCancel = () => {
+//     setShowPopup(false); // Hide the popup after canceling
+//   };
+
+//   return (
+//     <AuthContext.Provider
+//       value={{
+//         inputs,
+//         setInputs,
+//         validations,
+//         setValidations,
+//         loadings,
+//         setLoadings,
+//         new_device,
+//         setNew_device,
+//         handleInputs,
+//         getOTP,
+//         signUp,
+//         signIn,
+//         ssoAuthentication,
+//         addnewdevice,
+//         resetAuthValues,
+//       }}
+//     >
+//       {children}
+//       {showPopup && (
+//         <NewDevicePopup
+//           onConfirm={handlePopupConfirm}
+//           onCancel={handlePopupCancel}
+//         />
+//       )}
+//     </AuthContext.Provider>
+//   );
+// };
+// //   return (
+// //     <AuthContext.Provider
+// //       value={{
+// //         inputs,
+// //         setInputs,
+// //         validations,
+// //         setValidations,
+// //         loadings,
+// //         setLoadings,
+// //         new_device,
+// //         setNew_device,
+// //         handleInputs,
+// //         getOTP,
+// //         signUp,
+// //         ssoAuthentication,
+// //         addnewdevice,
+// //         resetAuthValues,
+// //       }}
+// //     >
+// //       {children}
+// //       {showPopup && (
+// //         <NewDevicePopup
+// //           onConfirm={handlePopupConfirm}
+// //           onCancel={handlePopupCancel}
+// //         />
+// //       )}
+// //     </AuthContext.Provider>
+// //   );
+// // };
+// export const useAuth = () => {
+//   return useContext(AuthContext);
+// };
+"use client";
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { ROOT_URL_AUTH } from "@/utlils/rootURL";
+import { handleError } from "@/utlils/handleError";
+import { MessageBox } from "@/components/MessageBox";
+import { getCookie } from "@/utlils/cookies";
+import NewDevicePopup from "@/components/auth/NewDevicePopup";
+
+const AuthContext = createContext();
 
 const defaultInputs = {
-  username: '',
-  name: '',
-  age: '',
-  email: '',
-  country_code: '',
-  phone: '',
-  otp: '',
-  phone_otp: ''
-}
+  username: "",
+  name: "",
+  age: "",
+  email: "",
+  country_code: "",
+  phone: "",
+  otp: "",
+  phone_otp: "",
+};
 
 const defaultValidations = {
   username: false,
@@ -28,164 +298,186 @@ const defaultValidations = {
   country_code: false,
   phone: false,
   otp: false,
-  phone_otp: false
-}
+  phone_otp: false,
+};
 
 const defaultLoadings = {
   otp: false,
   auth: false,
   google: false,
   meta: false,
-  microsoft: false
-}
+  microsoft: false,
+};
 
 export const AuthContextProvider = ({ children }) => {
-
-  const [inputs, setInputs] = useState(defaultInputs)
-  const [validations, setValidations] = useState(defaultValidations)
-  const [loadings, setLoadings] = useState(defaultLoadings)
-  const [verifier, setVerifier] = useState()
-  const [new_device, setNew_device] = useState(false)
+  const [inputs, setInputs] = useState(defaultInputs);
+  const [validations, setValidations] = useState(defaultValidations);
+  const [loadings, setLoadings] = useState(defaultLoadings);
+  const [new_device, setNew_device] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
   const handleInputs = (event) => {
-    const { name, value } = event.target
-    if (name === "username" && !USERNAME_REGEX.test(value)) return
-    if (name === "name" && !NAME_REGEX.test(value)) return
-    if ((name === "phone" || name === "otp" || name === "age" || name === "country_code" || name === "phone_otp") && !INT_NUMBER_REGEX.test(value)) return
-    setInputs(prev => ({ ...prev, [name]: value }))
-    setValidations(prev => ({ ...prev, [name]: false }))
-  }
+    const { name, value } = event.target;
+    setInputs((prev) => ({ ...prev, [name]: value }));
+    setValidations((prev) => ({ ...prev, [name]: false }));
+  };
 
-  const getOTP = async (action = 'sign-in') => {
-    try {      
-      setLoadings(prev => ({ ...prev, otp: true }))
-      const data = (action === 'sign-in') ? {
-        action,
-        email: inputs.email,
-      } :
-      {
-        action,
-        email: inputs.email,
+  const getOTP = async (action = "sign-in") => {
+    try {
+      setLoadings((prev) => ({ ...prev, otp: true }));
+      const data =
+        action === "sign-in"
+          ? {
+              action,
+              email: inputs.email,
+            }
+          : {
+              action,
+              email: inputs.email,
+              user_name: inputs.username,
+              name: inputs.name,
+              age: inputs.age,
+              email: inputs.email,
+              contact_number: inputs.phone,
+            };
+
+      const res = await axios.post(`${ROOT_URL_AUTH}/auth/get-otp`, data);
+      MessageBox("success", res.data.message);
+      return res;
+    } catch (err) {
+      handleError(err);
+    } finally {
+      setLoadings((prev) => ({ ...prev, otp: false }));
+    }
+  };
+
+  const signUp = async () => {
+    try {
+      setLoadings((prev) => ({ ...prev, auth: true }));
+      const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-up`, {
+        action: "sign-up",
         user_name: inputs.username,
         name: inputs.name,
         age: inputs.age,
         email: inputs.email,
         contact_number: inputs.phone,
-      }
-
-      const res = await axios.post(`${ROOT_URL_AUTH}/auth/get-otp`, data)
-      MessageBox('success', res.data.message)
-      return res
+        otp: inputs.otp,
+        phone_otp: inputs.phone_otp,
+      });
+      MessageBox("success", res.data.message);
+      return res;
     } catch (err) {
-      handleError(err)
+      handleError(err);
     } finally {
-      setLoadings(prev => ({ ...prev, otp: false }))
+      setLoadings((prev) => ({ ...prev, auth: false }));
     }
-  }
-
-  const signUp = async () => {
-    try {
-      setLoadings(prev => ({ ...prev, auth: true }))
-      const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-up`,
-        {
-          action: 'sign-up',
-          user_name: inputs.username,
-          name: inputs.name,
-          age: inputs.age,
-          email: inputs.email,
-          contact_number: inputs.phone,
-          otp: inputs.otp,
-          phone_otp: inputs.phone_otp
-        }
-      )
-      MessageBox('success', res.data.message)
-      return res
-    } catch (err) {
-      handleError(err)
-    } finally {
-      setLoadings(prev => ({ ...prev, auth: false }))
-    }
-  }
+  };
 
   const signIn = async () => {
     try {
-      setLoadings(prev => ({ ...prev, auth: true }))
+      setLoadings((prev) => ({ ...prev, auth: true }));
 
-      const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-in`,
-        {
-          action: 'sign-in',
-          email: inputs.email,
-          otp: inputs.otp,
-          phone_otp: inputs.phone_otp 
-        }
-      )
-      
-      setNew_device(res.data.new_device);
+      const res = await axios.post(`${ROOT_URL_AUTH}/auth/sign-in`, {
+        action: "sign-in",
+        email: inputs.email,
+        otp: inputs.otp,
+        phone_otp: inputs.phone_otp,
+      });
 
-      MessageBox('success', res.data.message)
-      return res
+      // if (res.data.new_device) {
+      setNew_device(true);
+      setShowPopup(true); // Show the popup after the new device is detected
+      // }
+
+      MessageBox("success", res.data.message);
+      return res;
     } catch (err) {
-      handleError(err)
+      handleError(err);
     } finally {
-      setLoadings(prev => ({ ...prev, auth: false }))
+      setLoadings((prev) => ({ ...prev, auth: false }));
     }
-  }
+  };
 
   const ssoAuthentication = async ({ provider, token }) => {
     try {
-      setLoadings(prev => ({ ...prev, [provider]: true }))
-      const res = await axios.post(`${ROOT_URL_AUTH}/auth/sso-authentication`,
-        { provider, token }
-      )
-      MessageBox('success', res.data.message)
-      return res
+      setLoadings((prev) => ({ ...prev, [provider]: true }));
+      const res = await axios.post(`${ROOT_URL_AUTH}/auth/sso-authentication`, {
+        provider,
+        token,
+      });
+      MessageBox("success", res.data.message);
+      return res;
     } catch (err) {
-      handleError(err)
+      handleError(err);
     } finally {
-      setLoadings(prev => ({ ...prev, [provider]: false }))
+      setLoadings((prev) => ({ ...prev, [provider]: false }));
     }
-  }
+  };
 
   const addnewdevice = async (email) => {
     try {
-      const res = await axios.post(`${ROOT_URL_AUTH}/user/addnewdevice`,
+      const res = await axios.post(
+        `${ROOT_URL_AUTH}/user/addnewdevice`,
         { email },
         {
           headers: {
-            Authorization: getCookie('token')
-          }
+            Authorization: getCookie("token"),
+          },
         }
-      )
-      MessageBox('success', res.data.message)
-      return res
+      );
+      MessageBox("success", res.data.message);
+      return res;
     } catch (error) {
-      handleError(err)
-    } 
-  }
+      handleError(err);
+    }
+  };
 
   const resetAuthValues = () => {
-    setInputs(prev => ({ ...prev, ...defaultInputs }))
-    setValidations(prev => ({ ...prev, ...defaultValidations }))
-    setLoadings(prev => ({ ...prev, ...defaultLoadings }))
-  }
+    setInputs(defaultInputs);
+    setValidations(defaultValidations);
+    setLoadings(defaultLoadings);
+  };
+
+  const handlePopupConfirm = async () => {
+    await addnewdevice(inputs.email); // Perform action on confirmation
+    setShowPopup(false); // Hide the popup after confirming
+  };
+
+  const handlePopupCancel = () => {
+    setShowPopup(false); // Hide the popup after canceling
+  };
 
   return (
-    <AuthContext.Provider value={{
-      inputs, setInputs,
-      validations, setValidations,
-      loadings, setLoadings,
-      new_device, setNew_device,
-      handleInputs,
-      getOTP, signUp, signIn,
-      ssoAuthentication,
-      addnewdevice,
-      resetAuthValues
-    }}>
+    <AuthContext.Provider
+      value={{
+        inputs,
+        setInputs,
+        validations,
+        setValidations,
+        loadings,
+        setLoadings,
+        new_device,
+        setNew_device,
+        handleInputs,
+        getOTP,
+        signUp,
+        signIn,
+        ssoAuthentication,
+        addnewdevice,
+        resetAuthValues,
+      }}
+    >
       {children}
+      {showPopup && (
+        <NewDevicePopup
+          onConfirm={handlePopupConfirm}
+          onCancel={handlePopupCancel}
+        />
+      )}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
 export const useAuth = () => {
-  return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
