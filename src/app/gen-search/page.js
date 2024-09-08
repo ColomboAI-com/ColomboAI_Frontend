3619d6e0baff8d6154975b3573d53559e76936a1
Sujatha Arunagiri <sarunagiri.dev@gmail.com>
2024-09-08 13:56:26 -0600

    Revert "Merge pull request #57 from ColomboAI-com/jamulya"
    This reverts commit 49067d5906e81e57aa6fed97c9b16c48d6a43f40, reversing
    changes made to ec21d2a02ec1ddfe81f0d9250d2225bf8def0dfb.

--- src/app/gen-search/page.js
+++ src/app/gen-search/page.js
@@ -1,53 +1,15 @@
-"use client";
-
-import React, { useState } from 'react';
-import InputBar from '../../components/layouts/InputBar.jsx';
-import RightSideIcons from '../../components/gen-ai/RightSideIcons.jsx';
-import Chat from '../../components/MassageInputActions/Chat.jsx';
-import HistoryChat from '@/components/MassageInputActions/HistoryChat.jsx';
+import React from 'react'
+import InputBar from '@/components/layouts/InputBar';
 
 function GenSearch() {
-  const [messages, setMessages] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [uploadedFile, setUploadedFile] = useState(null);
-
-   const sendMessage = (message, file) => {
-        setLoading(true);
-       
-        setTimeout(() => {
-          setMessages([...messages, 
-            { role: 'user', content: message, file: file },
-            { role: 'assistant', content: 'Scooby-Doo is a beloved animated series featuring a talking Great Dane and his teenage friends, Shaggy, Fred, Daphne, and Velma. Together, they form Mystery Inc. and embark on thrilling adventures to solve supernatural mysteries. With their iconic "Scooby-Dooby-Doo" catchphrase and wacky antics' }
-          ]);
-          setLoading(false);
-        }, 1000);
-      };
-
-  return (
-    <div className="flex flex-col min-h-screen">
-      <header className="sticky top-0 z-50 bg-white shadow-md">
-        <RightSideIcons />
-      </header>
-      <main className="flex-grow">
-        <div className="w-full lg:w-[70%] px-5 lg:px-20  mt-4">
-          <InputBar sendMessage={sendMessage} setUploadedFile={setUploadedFile} />
+    return (
+        <div>
+            
+            <div className="w-[100%] lg:w-[70%] px-5 lg:px-20 border-">
+                <InputBar />
+            </div>
         </div>
-        
-        {messages.length === 0 ? (
-          <p className="text-[15px] absolute font-[450] text-justify font-sans text-[#ACACAC] max-w-[806px] h-[60px] top-[216px] left-[192px] ">
-            Welcome to GenAI Search, your go-to tool for instant answers and web exploration! Simply type your question or topic of interest, and GenAI will provide you with accurate answers along with related links from the web. Whether you are seeking quick information or diving deeper into a topic, GenAI Search has you covered.
-          </p>
-        ) : (
-          <Chat
-            loading={loading}
-            messages={messages}
-            sendMessage={sendMessage}
-          />
-        )}
-        <HistoryChat/>
-      </main>
-    </div>
-  );
+    )
 }
 
-export default GenSearch;
+export default GenSearch
--- src/components/layouts/InputBar.jsx
+++ src/components/layouts/InputBar.jsx
@@ -1,136 +1,19 @@
-"use client";
+import { SendIcon } from "../Icons";
 
-import React, { useState, useRef, useEffect } from 'react';
-import { SendIcon, UploudIcon, PeeksIcon, RemoveIcon } from "../Icons";
-
-const InputBar = ({ sendMessage, setUploadedFile }) => {
-  const [message, setMessage] = useState('');
-  const [file, setFile] = useState(null);
-  const fileInputRef = useRef(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (message.trim() || file) {
-      sendMessage(message, file);
-      setMessage('');
-      setFile(null);
-      setUploadedFile(null);
-    }
-  };
-
-  const handleFileChange = (e) => {
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-    setUploadedFile(selectedFile);
-    // setMessage(`~${selectedFile.name}`);
-  };
-
-  const handleRemoveFile = () => {
-    setFile(null);
-    setUploadedFile(null);
-    if (fileInputRef.current) {
-      fileInputRef.current.value = '';
-    }
-  };
-  const borderStyle = {
-    border: '0.5px solid transparent',
-    backgroundClip: 'padding-box',
-    background: 'linear-gradient(white, white) padding-box, linear-gradient(180deg, #FF0049 0%, #FFBE3B 25%, #00BB5C 50%, #187DC4 75%, #58268B 100%) border-box',
-    borderImageSlice: 1,
-  };
-  
-
-  return (
-    //need this one
-//     <div className='relative'>
-//     <form onSubmit={handleSubmit} className="py-[21px]">
-//       <div className="relative w-full h-[50px] border-[0.5px] font-circular border-brandprimary rounded-[50px] overflow-hidden">
-//         <input
-//           type="text"
-//           value={message}
-//           onChange={(e) => setMessage(e.target.value)}
-//           placeholder="Ask or create anything..."
-//           className="w-full h-full py-[17px] px-[35px] pr-[100px] text-[#1E71F2] border-brandprimary text-[16px] tracking-[4px] font-sans overflow-hidden text-ellipsis whitespace-nowrap "
-//         />
-//         <div className="absolute top-0 right-0 h-full flex items-center pr-[25px]">
-//           <button
-//             type="button"
-//             onClick={() => fileInputRef.current.click()}
-//             className="mr-[20px]"
-//           >
-//             <UploudIcon w={20} h={17} />
-//           </button>
-//           <button type="submit">
-//             <SendIcon w={20} h={16} fill={message.trim() || file ? '#1E71F2' : '#1E71F2'} />
-//           </button>
-//         </div>
-//       </div>
-//       <input
-//         type="file"
-//         ref={fileInputRef}
-//         onChange={handleFileChange}
-//         className="hidden"
-//       />
-//       {file && (
-//         <div className="mt-2 flex items-center mr-2">
-//           <span className="text-[16px] text-[#1E71F2] font-circular text-base font-medium leading-[20.24px] tracking-[0.1em] mr-2">
-//             ~ {file.name}
-//           </span>
-//           <button type="button" onClick={handleRemoveFile}>
-//             <RemoveIcon w={12} h={12} />
-//           </button>
-//         </div>
-//       )}
-//     </form>
-//   </div>
-
-<div className='relative'>
-<form onSubmit={handleSubmit} className="py-[21px]">
-  <div 
-    className="relative w-full h-[50px] overflow-hidden font-circular font-sans focus-within:outline-none rounded-[25px]"
-    style={borderStyle}
-  >
-    <input
-      type="text"
-      value={message}
-      onChange={(e) => setMessage(e.target.value)}
-    //   placeholder={file ? '' : "Ask or create anything..."}
-       placeholder="Ask or create anything..."
-      className="w-full h-full py-[17px] px-[35px] pr-[100px] text-[#1E71F2] text-[16px] tracking-[0.1em] font-sans overflow-hidden text-ellipsis whitespace-nowrap bg-transparent focus:outline-none"
-    />
-    <div className="absolute top-0 right-0 h-full flex items-center pr-[25px]">
-      <button
-        type="button"
-        onClick={() => fileInputRef.current.click()}
-        className="mr-[20px]"
-      >
-        <UploudIcon w={20} h={17} />
-      </button>
-      <button type="submit">
-      <SendIcon w={20} h={16} fill={message.trim() || file ? '#1E71F2' : '#1E71F2'} />
-      </button>
-    </div>
-  </div>
-  <input
-    type="file"
-    ref={fileInputRef}
-    onChange={handleFileChange}
-    className="hidden"
-  />
-  {file && (
-    <div className="mt-4 flex items-center font-sans font-circular mr-2 absolute left-[37px]">
-      <span className="flex text-[16px] text-[#1E71F2] font-medium leading-[20.24px] tracking-[0.1em] mr-2">
-      <PeeksIcon w={16} h={16} className="mr-2 left-[-29px]" />
-        ~ {file.name}
-      </span>
-      <button type="button" onClick={handleRemoveFile} className='font-circular' >
-        <RemoveIcon w={12} h={12} />
-      </button>
-    </div>
-  )}
-</form>
-</div>
-  );
-};
+/* eslint-disable @next/next/no-img-element */
+const InputBar = () => {
+    return (
+        <div className="relative py-[21px]">
+            <input
+            type="text"
+            placeholder="Ask or create anything..."
+            className="w-full h-[50px] border-[1px] border-brandprimary rounded-[50px] py-[17px] px-[35px] text-[#ACACAC] text-[16px] tracking-[4px] font-sans"
+            ></input>
+            <button className="absolute top-[39px] right-[34px]">
+                <SendIcon w={20} h={16} fill={'#1E71F2'} />
+            </button>
+      </div>
+    );
+}
 
 export default InputBar;
