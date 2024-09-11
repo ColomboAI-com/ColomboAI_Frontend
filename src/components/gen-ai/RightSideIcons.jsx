763b443878a05e82591a2c94a6ddffebf62b5dd6
vinay2277 <vinayvyankatkar3@gmail.com>
2024-09-11 23:22:29 +0530

    Made Some Chnages in History

--- src/components/gen-ai/RightSideIcons.jsx
+++ src/components/gen-ai/RightSideIcons.jsx
@@ -1,15 +1,15 @@
 "use client";
 import React from 'react';
 import { HistoryIcon, SettingIcon } from "../Icons";
-
+import Link from 'next/link';
 
 
 const RightSideIcons = () => {
     return (
       <div className="fixed top-[113px] left-[1281px] w-[83px] h-[27px] flex items-center justify-between">
-      <button className="">
+      <Link  className="" href={"/history"}>
         <HistoryIcon w={29} h={27}/>
-      </button>
+      </Link>
       <button className=" ">
         <SettingIcon w={26} h={26}/>
       </button>
