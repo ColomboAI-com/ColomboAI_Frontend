a38ac0068abe133866368336f29fc1744ebb7d0a
ForumShah8 <shahforu00@gmail.com>
2024-09-24 16:49:04 -0400

    navbar links working

--- src/app/(default)/(feed)/layout.js
+++ src/app/(default)/(feed)/layout.js
@@ -9,16 +9,18 @@ const FeedLayout = ({ children }) => {
   const { isCommentOpen } = useContext(GlobalContext);
 
   return (
-    <UserProfileContextProvider>
-      {/* <div className="border-green-500 flex flex-row justify-center"> */}
+    <UserProfileContextProvider
+    >
+          <FeedFilter className="bg-white sticky  top-0 z-10 py-[14px] sm2:py-3 flex flex-wrap items-center justify-evenly border- px-6 lg:px-16 gap-6 shadow-[0px_2px_4px_0px_#0000001A]" />
+      <div className="border-green-500 flex flex-row justify-center">
         {/* <p className="mt-[1.25rem]">Coming Soon</p> */}
-        <FeedFilter className="bg-white sticky  top-0 z-10 py-[14px] sm2:py-3 flex flex-wrap items-center justify-evenly border- px-6 lg:px-16 gap-6 shadow-[0px_2px_4px_0px_#0000001A]" />
-        <Feed/>
-      
-        {/* <div className="w-[100%] h-[calc(100vh-263px)] overflow-y-auto px-6 md:px-10 lg:px-20 " id="feed_section">
+       
+        {/* <Feed/> */}
+    
+        <div className="w-[100%] h-[calc(100vh-263px)] overflow-y-auto px-6 md:px-10 lg:px-20 " id="feed_section">
           {children}
-        </div> */}
-      {/* </div> */}
+        </div>
+      </div>
     </UserProfileContextProvider>
   );
 };
