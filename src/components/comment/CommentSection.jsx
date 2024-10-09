8eb1a4fa9b7bfc7d6b2a24921abbe2c6f49cbaef
Zax-boop <rohan.arya01@gmail.com>
2024-10-09 14:27:54 -0500

    fixed comment section

--- src/components/comment/CommentSection.jsx
+++ src/components/comment/CommentSection.jsx
@@ -184,32 +184,33 @@ const CommentSection = ({ specificPostId, posts }) => {
   }, [pickerRef]);
 
   return (
-    <div className="bg-[black] py-20 xl:flex w-full xl:overflow-hidden lg:flex-row lg:h-full  md:flex-col md:overflow-auto sm:flex-col sm:overflow-auto">
-      <div className="h-full w-full">
+    <div className="flex flex-row justify-center">
+      <div className="bg-[black] sm:h-[0rem] xl:h-[40rem] xl:flex sm:w-[0rem] xl:w-full xl:overflow-hidden ">
+        <div className="h-full w-full">
 
-        {posts?.type === "image" && (
-          <img
-            src={posts?.media[0]}
-            className="w-full h-full aspect-video object-contain"
-          />
-        )}
-        {posts?.type === "video" && (
-          <video
-            className="inset-0 w-full h-full aspect-video"
-            src={posts?.media[0]}
-            controls
-          >
-            Your browser does not support the video tag.
-          </video>
-        )}
-        {posts?.type === "" && (
-          <img
-            src="/images/home/feed-banner-img.png"
-            className="w-full h-full aspect-video"
-          />
-        )}
-      </div>
-      {/* <div className="xl:block w-[60%] xl:w-[70%] xl:h-[85vh] lg:h-screen md:w-full sm:w-full sm:hidden">
+          {posts?.type === "image" && (
+            <img
+              src={posts?.media[0]}
+              className="w-full h-full aspect-video object-contain"
+            />
+          )}
+          {posts?.type === "video" && (
+            <video
+              className="inset-0 w-full h-full aspect-video"
+              src={posts?.media[0]}
+              controls
+            >
+              Your browser does not support the video tag.
+            </video>
+          )}
+          {posts?.type === "" && (
+            <img
+              src="/images/home/feed-banner-img.png"
+              className="w-full h-full aspect-video"
+            />
+          )}
+        </div>
+        {/* <div className="xl:block w-[60%] xl:w-[70%] xl:h-[85vh] lg:h-screen md:w-full sm:w-full sm:hidden">
         <div className="h-full  flex items-center relative min-w-[651px] max-w-[1200px] xl:w-full lg-max:w-[651px]">
           <button
             onClick={() => setIsCommentOpen(false)}
@@ -220,14 +221,16 @@ const CommentSection = ({ specificPostId, posts }) => {
         </div>
 
       </div> */}
-      <div className="lg:w-[40%] bg-white px-4 xl:w-[40%] xl:sm:z-[0] xl:relative xl:h-[85vh] md:w-full md:left-[0] sm:w-full sm:absolute sm:z-[99] sm:left-0 sm:top-auto sm:bottom-0 md:h-[70vh] md:top-auto md:bottom-0">
+
+      </div>
+      <div className="xl:w-[40%] md:w-[40rem] sm:w-[20rem] overflow-y-scroll sm:h-[30rem] md:h-[40rem] bg-white px-4">
         <div class="flex items-center justify-between px-[16px] py-[12px]">
           <a
             class="flex items-center"
             target="_blank"
             href={`/profile/${posts?.creator?.user_name}`}
           >
-            <ProfilePicture image={posts?.creator?.profile_picture} />
+            <ProfilePicture image={posts?.creator?.profile_picture} size={'w-[2rem]'}/>
             <p class="text-[18px] font-sans font-[700] text-[#242424] pl-[17px]">
               {posts?.creator?.user_name}
             </p>
@@ -243,8 +246,8 @@ const CommentSection = ({ specificPostId, posts }) => {
             {posts?.content}
           </p>
         </div>
-        <div className="flex h-[10%] items-center justify-center">
-          <h4 className="text-[21px] color-[#333333] font-sans font-[700]">
+        <div className="flex mt-2 items-center justify-center">
+          <h4 className="text-[15px] color-[#333333] font-sans font-[700]">
             Comments
           </h4>
           <div></div>
