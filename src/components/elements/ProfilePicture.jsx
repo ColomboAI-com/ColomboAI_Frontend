0838b037347c1aea4364bfc73b2ba5c5877f4827
ForumShah8 <shahforu00@gmail.com>
2024-10-09 13:46:45 -0400

    side bar ui fixes

--- src/components/elements/ProfilePicture.jsx
+++ src/components/elements/ProfilePicture.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function ProfilePicture({ image = '/images/profile/defalut_user.svg', size = 42, className }) {
+export default function ProfilePicture({ image = '/images/profile/defalut_user.svg', size , className }) {
   return (
     <img
       src={image}
       alt="avatar"
-      className={`rounded-full${className ? ` ${className}` : ''}`}
-      style={{ height: size, width: size }}
+      className={`rounded-full${className ? ` ${className}` : ''} ${size}`}
+      // style={{ height: size, width: size }}
     />
   )
 }
