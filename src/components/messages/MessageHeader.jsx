abf36115629cf9478a93a4626dfe41af7ba955b0
vinay2277 <vinayvyankatkar3@gmail.com>
2024-10-10 22:51:46 +0530

    removed banner ad from vibes

--- src/components/messages/MessageHeader.jsx
+++ src/components/messages/MessageHeader.jsx
@@ -1,26 +1,31 @@
-import Link from "next/link"
-import { ChatBubbleIcon, NewChatIcon, NotificationIcon, SearchIcon } from "../Icons"
-import CreateDropdown from "../elements/CreateDropdown"
-import { usePathname } from "next/navigation"
-import { GlobalContext } from "@/context/GlobalContext"
-import { useContext } from "react"
+import { useState, useContext } from "react";
+import Link from "next/link";
+import { ChatBubbleIcon, NewChatIcon, NotificationIcon, SearchIcon } from "../Icons";
+import CreateDropdown from "../elements/CreateDropdown";
+import { usePathname } from "next/navigation";
+import { GlobalContext } from "@/context/GlobalContext";
+import SearchAll from './searchall'; // Import your SearchAll component
 
 const MessageHeader = () => {
+  const { setIsNewMessageOpen } = useContext(GlobalContext);
+  const pathname = usePathname();
+  const [showSearchPage, setShowSearchPage] = useState(false); // State to toggle SearchAll visibility
 
-  const { setIsNewMessageOpen } = useContext(GlobalContext)
-  const pathname = usePathname()
+  const handleSearchClick = () => {
+    setShowSearchPage(true); // Show the SearchAll component when SearchIcon is clicked
+  };
 
   return (
     <div className=" border-y-[2px] py-[14px] z-50 bg-white ">
-      <div className=" hidde flex items-center justify-between mx-2 lg:mx-4">
+      <div className="hidde flex items-center justify-between mx-2 lg:mx-4">
         <div className="flex w-[30%] md:w-[305px] justify-between">
-          <p className=' hidden lg:block text-2xl font-semibold '>Messages</p>
+          <p className='hidden lg:block text-2xl font-semibold'>Messages</p>
           <button onClick={() => setIsNewMessageOpen(true)} className="text-[#646464] hover:text-brandprimary">
             <NewChatIcon w={31} h={31} fill={'currentcolor'} />
           </button>
         </div>
-        <div className="flex gap-2 lg:gap-4 xl:gap-6 mr-4 justify-evenly items-baseline  ">
-          <button className="text-[#646464] hover:text-brandprimary">
+        <div className="flex gap-2 lg:gap-4 xl:gap-6 mr-4 justify-evenly items-baseline">
+          <button className="text-[#646464] hover:text-brandprimary" onClick={handleSearchClick}>
             <SearchIcon w={30} h={30} fill={'currentcolor'} />
           </button>
           <div className="text-[#646464] hover:text-brandprimary">
@@ -36,8 +41,15 @@ const MessageHeader = () => {
           </button>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default MessageHeader
+      {/* Conditionally render the SearchAll component based on the showSearchPage state */}
+      {showSearchPage && (
+        <div className="mt-4">
+          <SearchAll />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MessageHeader;
