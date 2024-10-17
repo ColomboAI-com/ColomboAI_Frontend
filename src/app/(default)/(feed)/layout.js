717fa2ddc49382d1dcc358f400cc3cfd425d333c
Zax-boop <rohan.arya01@gmail.com>
2024-10-16 20:04:16 -0500

    made general mobile feed fixes like input bar search fix, rearranging mobile header, and making feed filter sticky

--- src/app/(default)/(feed)/layout.js
+++ src/app/(default)/(feed)/layout.js
@@ -10,15 +10,14 @@ const FeedLayout = ({ children }) => {
 
   return (
     <UserProfileContextProvider>
-      {/* <div className="border-green-500 flex flex-row justify-center"> */}
-        {/* <p className="mt-[1.25rem]">Coming Soon</p> */}
-        <div className="w-full mx-auto flex justify-center ">
-        <FeedFilter className="bg-white w-full sticky top-0 z-10 sm:pb-[6px] sm:pt-[8px] lg:pb-[14px] lg:pt-[14px] sm2:py-3 flex flex-wrap items-center justify-evenly xl:px-6 lg:px-0 lg:gap-8 xl:gap-6 shadow-[0px_2px_4px_0px_#0000001A]" />
+      <div className="w-full mx-auto bg-white">
+        <div className="sticky top-0 z-10 w-full bg-white shadow-md">
+          <FeedFilter className="w-full sm:pb-[6px] sm:pt-[8px] lg:pb-[14px] lg:pt-[14px] flex flex-wrap items-center justify-evenly xl:px-6 lg:px-0 lg:gap-8 xl:gap-6 shadow-[0px_2px_4px_0px_#0000001A]" />
         </div>
-        <div className="w-[100%] overflow-y-auto md:px-10 lg:px-0 xl:px-20 " id="feed_section">
+        <div className="w-full overflow-y-auto md:px-10 lg:px-0 xl:px-20" id="feed_section">
           {children}
         </div>
-      {/* </div> */}
+      </div>
     </UserProfileContextProvider>
   );
 };
