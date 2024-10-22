59e58750c76ec4999ee5b61da4b53f999c4f5f10
Su-24-TTu <muchelisulakshana25@gmail.com>
2024-10-21 19:39:41 -0500

    formatting the tag people

--- src/components/elements/CaptionBox.jsx
+++ src/components/elements/CaptionBox.jsx
@@ -150,13 +150,13 @@ const CaptionBox = ({ captionInput, setCaptionInput, width, handleCreateVibe }) 
                       }`}
                     >
                       <div >
-                       <Image
+                       {/* <Image
                         src={user.profile_picture} 
                         alt={`${user.user_name}'s profile`}
                         width={40} 
                         height={40}
                         className="rounded-full mr-2" 
-                       />
+                       /> */}
                         <div>
                         <strong>@{user.user_name}</strong> {/* Bold username */}
                         <div className="text-sm text-gray-500">{user.name}</div> {/* Name below the username */}
