f582973fb546e248a8074bcca1281c2a660d2da4
Zax-boop <rohan.arya01@gmail.com>
2024-10-24 14:36:40 -0500

    successfully added vibes switching for desktop but mobile doesn't work as well

--- src/app/(default)/(feed)/layout.js
+++ src/app/(default)/(feed)/layout.js
@@ -4,9 +4,13 @@ import FeedFilter from "@/components/layouts/FeedFilter";
 import { GlobalContext } from "@/context/GlobalContext";
 import UserProfileContextProvider from "@/context/UserProfileContext";
 import { useContext } from "react";
+import { usePathname } from "next/navigation";
+
 
 const FeedLayout = ({ children }) => {
   const { isCommentOpen } = useContext(GlobalContext);
+  const pathname = usePathname();
+
 
   return (
     <UserProfileContextProvider>
@@ -14,7 +18,7 @@ const FeedLayout = ({ children }) => {
         <div className="sticky top-0 z-10 w-full bg-white shadow-md">
           <FeedFilter className="w-full sm:pb-[6px] sm:pt-[8px] lg:pb-[14px] lg:pt-[14px] flex flex-wrap items-center justify-evenly xl:px-6 lg:px-0 lg:gap-8 xl:gap-6 shadow-[0px_2px_4px_0px_#0000001A]" />
         </div>
-        <div className="w-full overflow-x-clip overflow-y-auto md:px-10 lg:px-0 xl:px-20" id="feed_section">
+        <div className={`w-full overflow-x-clip overflow-y-auto ${pathname === `/vibes` ? `md:px-0` : `md:px-10`} lg:px-0 ${pathname !== '/vibes' && `xl:px-20`}`} id="feed_section">
           {children}
         </div>
       </div>
--- src/app/(default)/(feed)/vibes/page.js
+++ src/app/(default)/(feed)/vibes/page.js
@@ -4,21 +4,79 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { VibeContext } from "@/context/VibeContext";
 import Vibe from "@/components/feed/vibes/Vibe";
+import Slider from "react-slick";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+var settings = {
+  dots: false,
+  arrows: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  vertical: true,
+  verticalSwiping: true,
+};
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
 
 export default function Vibes({ filter }) {
   const { vibes, getVibes, fetchSongById } = useContext(VibeContext);
   const [song, setSong] = useState({});
   const [vibe, setVibe] = useState({});
-  
+
   useEffect(() => {
     getVibes();
   }, []);
 
   return (
-    <div>
-      {vibes.map((vibe) => (
-        <Vibe vibe={vibe} key={vibe._id} />
-      ))}
-    </div>
+    <>
+      <div className="w-full md:h-[38rem] lg:h-full xl:h-[34.6rem] sm:hidden md:block bg-black"> {/* Ensure full-screen container */}
+        <Carousel
+          swipeable={false}
+          draggable={false}
+          showDots={false}
+          responsive={responsive}
+          ssr={true}
+          autoPlay={false}
+          infinite={true}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          // containerClass="carousel-container"
+          removeArrowOnDeviceType={["mobile"]}
+          dotListClass="custom-dot-list-style"
+          itemClass="w-full h-full" // Full-screen item
+        >
+          {vibes.map((vibe) => (
+            <Vibe vibe={vibe} key={vibe._id} />
+          ))}
+        </Carousel>
+      </div>
+      <div className="w-full md:h-[38rem] lg:h-full xl:h-[34.6rem] md:hidden bg-black">
+        <Slider {...settings}>
+          {vibes.map((vibe) => (
+            <Vibe vibe={vibe} key={vibe._id} />
+          ))}
+        </Slider>
+      </div>
+    </>
   );
 }
