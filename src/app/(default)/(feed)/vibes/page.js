7772fd5a0588519b240fe7059c93a02351d3a21e
Tuan Nguyen <trentontuannguyen@gmail.com>
2024-10-23 20:43:39 -0700

    move music playing into Vibe component

--- src/app/(default)/(feed)/vibes/page.js
+++ src/app/(default)/(feed)/vibes/page.js
@@ -9,40 +9,10 @@ export default function Vibes({ filter }) {
   const { vibes, getVibes, fetchSongById } = useContext(VibeContext);
   const [song, setSong] = useState({});
   const [vibe, setVibe] = useState({});
-  const audioRef = useRef(null);
-
-  useEffect(() => {
-    audioRef.current = typeof Audio !== 'undefined' ? new Audio() : null;
-  }, []);
   
   useEffect(() => {
     getVibes();
-
-    const fetchSong = async () => {
-      try {
-        // const result = await fetchSongById("1295528");
-        const result = await fetchSongById();
-        setSong(result[0]);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchSong();
   }, []);
-
-  useEffect(() => {
-    if (vibes) {
-      setVibe(vibes[0]);
-    }
-  })
-
-  useEffect(() => {
-    if (audioRef.current && song && song.audio) {
-      audioRef.current.src = song.audio;
-      audioRef.current.play().catch((error) => console.error("Error playing audio:", error));
-    }
-  }, [song]);
-  // console.log(song);
 
   return (
     <div>
