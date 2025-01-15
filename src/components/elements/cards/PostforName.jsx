251c038c813bf760420c4375cc33732a97ddd2f9
philip-luther <philipluther0423@gmail.com>
2025-01-15 08:20:54 -0600

    Added search functionality feature

---
+++ src/components/elements/cards/PostforName.jsx
@@ -0,0 +1,132 @@
+// import { useContext } from "react";
+// import RecentComments from "@/components/feed/post/RecentComments";
+// import ImageBlock from "@/components/feed/post/ImageBlock";
+// import PostActions from "@/components/feed/post/PostActions";
+// import ProfilePicture from "../ProfilePicture";
+// import Username from "../Username";
+// import { formatTimeAgo } from "@/utlils/commonFunctions";
+// import ContentBlock from "@/components/feed/post/ContentBlock";
+// import VideoBlock from "@/components/feed/post/VideoBlock";
+// import Link from "next/link";
+// import {
+//   ExclamationIcon,
+//   PostMoreOptionsIcon,
+//   ReportIcon,
+//   RestrictUserIcon,
+//   UserProfileIcon,
+// } from "@/components/Icons";
+// import Dropdown from "@/components/messages/Dropdown";
+// import { FeedContext } from "@/context/FeedContext";
+// import { UserProfileContext } from "@/context/UserProfileContext";
+// import { getCookie } from "@/utlils/cookies";
+// import ThreeDotMenu from "../ThreeDotMenu";
+// import { useEffect } from "react";
+// import { InfoIcon, SaveIcon } from "lucide-react";
+// import { ExclamationCircleIcon } from "@heroicons/react/solid";
+import PostCardforName from "./PostCardforName";
+import RePostCard from "./RePostCard";
+
+const PostforName = ({ post, index, CreatorName}) => {
+  // const { deletePost } = useContext(FeedContext);
+
+  // const { userDetails } = useContext(UserProfileContext);
+
+  // const handleDeletePost = async () => {
+  //   try {
+  //     const response = await deletePost(post._id);
+  //   } catch (error) {
+  //     console.error("Failed to delete post:", error);
+  //   }
+  // };
+
+  return (
+    <>
+      {!(post.repostBy) ? <PostCardforName post={post} CreatorName={CreatorName}/> : <RePostCard post={post}/> }
+      {/* <div className="flex items-center">
+        <ProfilePicture image={post?.creator?.profile_picture} className="w-[20px] h-[20px]" />
+        <Username username={post?.creator?.user_name} className="text-[12px] pl-[7px]" /><span className="text-[#b3b3b3] font-sans"> reposted this</span>
+      </div> */}
+
+      {/* <div className={`overflow-x-hidden border-[0.5px] ${index == 0 ? `border-black` : `border-brandprimary`} sm:rounded-none md:rounded-[10px] mt-5`}>
+        <div className="flex lg:flex-row md:flex-row flex-col items-center justify-between px-[16px] py-[10px]">
+          <Link
+            className="flex items-center justify-start w-full md:w-fit lg:w-fit"
+            href={`/profile/${post?.creator?.user_name || ""}`}
+            target="_blank"
+          >
+            <ProfilePicture image={post?.creator?.profile_picture} size={"w-[2rem]"} />
+            <Username username={post?.creator?.user_name} className="text-[12px]" />
+          </Link>
+          <div className="flex items-center gap-4 justify-between pl-2 lg:w-fit md:w-fit w-full">
+            <p className="font-sans text-sidebarlabel text-[12px] text-[#8B8B8B]">
+              {formatTimeAgo(post?.createdAt)}
+            </p>
+
+            <Dropdown
+              offset={[0, 10]}
+              placement="bottom-start"
+              btnClassName="flex justify-center items-center rounded-full hover:text-brandprimary cursor-pointer"
+              button={<PostMoreOptionsIcon w={30} h={30} fill={"#A7A7A7"} />}
+            >
+              {userDetails?.user_name === post?.creator?.user_name ? (
+                <ul className="rounded bg-white shadow-md text-center">
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Archive</li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Edit</li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Hide Like Counts</li>
+                  <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Turn Off Commenting</li>
+                  <li
+                    className="px-4 py-2 text-red-600 hover:bg-gray-100 cursor-pointer"
+                    onClick={handleDeletePost}
+                  >
+                    Delete
+                  </li>
+                </ul>
+              ) : (
+                <ul className="rounded bg-white shadow-md">
+                  <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    <SaveIcon w={25} h={25} fill={"currentcolor"} />
+                    <span className="ml-2">Save</span>
+                  </li>
+                  <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    <RestrictUserIcon w={25} h={25} fill={"currentcolor"} />
+                    <span className="ml-2">Unfollow</span>
+                  </li>
+                  <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    <SaveIcon w={25} h={25} fill={"currentcolor"} />
+                    <span className="ml-2">Hide</span>
+                  </li>
+                  <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    <InfoIcon w={25} h={25} fill={"currentcolor"} />
+                    <span className="ml-2">Why are you seeing this</span>
+                  </li>
+                  <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+                    <UserProfileIcon w={25} h={25} fill={"currentcolor"} />
+                    <span className="ml-2">About this account</span>
+                  </li>
+                  <li className="flex items-center px-4 py-2 text-red-600 hover:bg-gray-100 cursor-pointer">
+                    <ReportIcon w={25} h={25} fill={"currentcolor"} />
+                    <span className="ml-2">Report</span>
+                  </li>
+                </ul>
+              )}
+            </Dropdown>
+          </div>
+        </div>
+
+        {post?.type === "image" && <ImageBlock image={post.media} />}
+        {post?.type === "video" && <VideoBlock video={post.media} />}
+        {post?.content && <ContentBlock content={post.content} />}
+        <div className="px-[12px] py-[2px]">
+          {post && (
+            <>
+              <PostActions post={post} />
+              <RecentComments comments={post.comments} />
+            </>
+          )}
+        </div>
+      </div> */}
+    </>
+  );
+};
+
+export default PostforName;
---
+++ src/components/feed/post/RenderFeedBuName.jsx
@@ -0,0 +1,161 @@
+"use client";
+import FooterAdComponent from "@/components/ads/Ad";
+import PostforName from "@/components/elements/cards/PostforName";
+import Loader from "@/components/Loader";
+import NoDataFound from "@/components/NoDataFound";
+import { feed, FeedContext } from "@/context/FeedContext";
+import { useContext, useEffect, Fragment, useRef } from "react";
+import { getCookie } from "@/utlils/cookies";
+import { UserProfileContext } from "@/context/UserProfileContext";
+import SuggestedVibes from "@/components/layouts/SuggestedVibes";
+import LargeAdComponent from "@/components/ads/LargeAd";
+import VideoAd from "@/components/ads/VideoAd";
+
+export default function RenderFeedBuName({ filter , CreatorName}) {
+  const { posts, getPosts, loadings, page, resetFeedValues } = useContext(FeedContext);
+
+  const { getUserDetails } = useContext(UserProfileContext);
+
+  // const [isFetchingMore, setIsFetchingMore] = useState(false);
+  // This reference is attached to the 'Load More' button
+  const loadMoreRef = useRef(null);
+
+  const userName = getCookie("username");
+
+  useEffect(() => {
+    getPosts(filter);
+    return () => resetFeedValues();
+  }, [filter]);
+
+  useEffect(() => {
+    getUserDetails(userName);
+    return () => resetFeedValues();
+  }, [userName]);
+  // const handleFeedScroll = () => {
+  //   const feedSection = document.getElementById("feed_section");
+  //   console.log("SCROLL TRIGGERD");
+  //   if (
+  //     feedSection &&
+  //     !loadings.getPost &&
+  //     Math.ceil(feedSection.scrollTop + feedSection.clientHeight) === feedSection.scrollHeight
+  //   )
+  //     getPosts(filter, page);
+  // };
+
+  // useEffect(() => {
+  //   const feedSection = document.getElementById("feed_section");
+  //   console.log("POST INFY", feedSection);
+  //   feedSection?.addEventListener("scroll", handleFeedScroll);
+  //   return () => {
+  //     feedSection?.removeEventListener("scroll", handleFeedScroll);
+  //   };
+  // }, [page, loadings.getPost]);
+
+  // INFINTIE SCROLLING NEW CODE - START
+  const handleLoadMore = () => {
+    if (!loadings.getPost) {
+      getPosts(filter, page);
+    }
+  };
+
+  useEffect(() => {
+    // Intersection Observer to automatically call handleLoadMore when the button is in view
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          handleLoadMore(); // Automatically trigger the function when the button is visible
+        }
+      },
+      {
+        root: null, // Uses the browser viewport as the default
+        rootMargin: "0px",
+        threshold: 1.0, // Trigger when 100% of the button is visible
+      }
+    );
+
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current);
+    }
+
+    // Cleanup the observer when the component unmounts or if button changes
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current);
+      }
+    };
+  }, [page, loadings.getPost]);
+
+  // INFINTIE SCROLLING NEW CODE - END
+
+  useEffect(() => {
+    // Your existing useEffect logic here
+
+    // const feedSection = document.getElementById("posts_container_infy_scroll");
+
+    return () => {
+      // Cleanup function to destroy ad slots
+      posts.forEach((_, index) => {
+        if ((index + 1) % 4 === 0) {
+          const adSlotId = `feed-ad-${index}`;
+          const adSlot = window.googletag
+            .pubads()
+            .getSlots()
+            .find((slot) => slot.getSlotElementId() === adSlotId);
+          if (adSlot) {
+            window.googletag.destroySlots([adSlot]);
+          }
+        }
+      });
+    };
+  }, [page, loadings.getPost, posts]);
+
+  if (loadings.getPost && !posts.length) return <Loader className={"mt-5"} />;
+
+  // // Function to handle the ad click
+  // const handleAdClick = (index) => {
+  //   // Collect the previous 4 postIds when the ad is clicked
+  //   const postIds = posts.slice(index - 4, index).map((post) => post._id);
+  //   const adRevenue = 100; // TODO - adRevenue how to calc?
+
+  //   generateWallet(postIds, adRevenue);
+  // };
+
+  return (
+    <div className="sm:px-0 md:px-0" id="posts_container_infy_scroll">
+      {posts.length ? (
+        posts.map((i, index) => (
+          <Fragment key={index}>
+            <PostforName post={i} index={index} CreatorName={CreatorName}/>
+            {(index + 1) % 4 === 0 && (
+              <>
+                {Math.floor((index + 1) / 4) % 2 === 0 ? (
+                  <div className="overflow-x-hidden rounded-[10px] mt-5">
+                    <div className="flex lg:flex-row md:flex-row flex-col items-center justify-between py-[12px]">
+                      <VideoAd />
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    className="overflow-x-hidden rounded-[10px] mt-5"
+                    onClick={() => handleAdClick(index + 1)}
+                  >
+                    <div className="flex lg:flex-row md:flex-row flex-col items-center justify-between py-[12px]">
+                      <LargeAdComponent divid={`feed-ad-${index}`} />
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+          </Fragment>
+        ))
+      ) : (
+        <NoDataFound className={"mt-5"} />
+      )}
+      {/* Invisible Load More Button */}
+      <div ref={loadMoreRef} style={{ height: "1px" }}></div>
+      <div style={{ color: "transparent" }}>Hello</div>
+      {/* KEEP THIS DIV WITH HELLO SO THAT THE BUTTON APPEARS IN THE WINDOW FOR INFINITE SCROLL TO WORK */}
+    </div>
+  );
+}
