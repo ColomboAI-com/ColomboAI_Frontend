351ac56cae180aad88cac3754dc48bedaa11d4d2
sarunagiri-dev <sarunagiri.dev@gmail.com>
2025-02-19 11:59:10 -0700

    prod url rootURL.js update

--- src/utlils/rootURL.js
+++ src/utlils/rootURL.js
@@ -1,14 +1,15 @@
-// LOCAL URLS
-// export const ROOT_URL_AUTH = "http://localhost:8000";
-// export const ROOT_URL_FEED = "http://localhost:8001";
-// export const ROOT_URL_MESSAGES = "http://localhost:8001";
-// export const ROOT_URL_NOTIFICATION = "http://localhost:8002";
+// export const ROOT_URL_AUTH = 'https://userservice-dot-fair-myth-398920.uc.r.appspot.com'
+// export const ROOT_URL_FEED = 'https://feedservice-dot-fair-myth-398920.uc.r.appspot.com'
 
-// PUBLIC URLS
-export const ROOT_URL_AUTH = "https://caiuserservice-1-dev-dot-fair-myth-398920.uc.r.appspot.com";
-export const ROOT_URL_FEED = "https://caifeedservice-1-dev-dot-fair-myth-398920.uc.r.appspot.com";
-export const ROOT_URL_MESSAGES = "https://cai-messaging-dev-dot-fair-myth-398920.uc.r.appspot.com";
-export const ROOT_URL_NOTIFICATION = "https://notificationservice-dev-dot-fair-myth-398920.uc.r.appspot.com";
+// export const ROOT_URL_AUTH = "http://localhost:8000";
+// export const ROOT_URL_FEED = 'http://localhost:8001'
+
+export const ROOT_URL_AUTH = "https://caiuserservice-prod-dot-fair-myth-398920.uc.r.appspot.com";
+export const ROOT_URL_FEED = "https://caifeedservice-prod-dot-fair-myth-398920.uc.r.appspot.com";
+
+export const ROOT_URL_MESSAGES = "https://cai-messaging-prod-dot-fair-myth-398920.uc.r.appspot.com";
+export const ROOT_URL_NOTIFICATION = "https://cai-notifications-prod-dot-fair-myth-398920.uc.r.appspot.com";
+
 export const ROOT_URL_LLM = "https://magicpen.colomboai.com";
 
 export const COOKIE_OPTIONS = {
