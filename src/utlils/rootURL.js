541f1dd48db12432bb00d5a8d3e68637e8f1023e
Ashwin <ashwinfury1@gmail.com>
2025-05-02 21:05:26 -0400

    grpc implementation to fetch posts based on recommendations done

--- src/utlils/rootURL.js
+++ src/utlils/rootURL.js
@@ -3,6 +3,7 @@
 // export const ROOT_URL_FEED = "http://localhost:8001";
 // export const ROOT_URL_MESSAGES = "http://localhost:8001";
 // export const ROOT_URL_NOTIFICATION = "http://localhost:8002";
+// export const ROOT_URL_RECOMMENDATION_GRPC = "localhost:50051";
 
 // PUBLIC URLS
 export const ROOT_URL_AUTH = "https://caiuserservice-1-dev-dot-fair-myth-398920.uc.r.appspot.com";
@@ -10,6 +11,7 @@ export const ROOT_URL_FEED = "https://caifeedservice-1-dev-dot-fair-myth-398920.
 export const ROOT_URL_MESSAGES = "https://cai-messaging-dev-dot-fair-myth-398920.uc.r.appspot.com";
 export const ROOT_URL_NOTIFICATION = "https://cai-notifications-dev-dot-fair-myth-398920.uc.r.appspot.com/";
 export const ROOT_URL_LLM = "https://magicpen-dev.colomboai.com";
+export const ROOT_URL_RECOMMENDATION_GRPC = "35.232.68.95:50051";
 
 export const COOKIE_OPTIONS = {
   path: "/",
